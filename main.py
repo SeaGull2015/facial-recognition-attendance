3bba738c942b55a575a06fe07f8112b73d55e026
dmitry <wk.zaytsev.dv@gmail.com>
2022-11-28 10:31:39 +0300

    made cv2 webcam video show in kivy window

--- main.py
+++ main.py
@@ -1,12 +1,61 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+import cv2 # opencv-python - requires numpy
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.image import Image
+from kivy.clock import Clock
+from kivy.graphics.texture import Texture
 
-class MainWidget(Widget):
+class RecognitionWidget(BoxLayout):
     pass
+    
 
 class RecognitionApp(App):
     def build(self):
-        return MainWidget()
+        self.img1 = Image()
+        layout = BoxLayout()
+        layout.add_widget(self.img1)
+        # opencv2 stuffs
+        self.capture = cv2.VideoCapture(0)
+        Clock.schedule_interval(self.update, 1.0 / 33.0)
+        return layout
+        #return RecognitionWidget()
+
+    def update(self, dt):
+        # display image from cam in opencv window
+        ret, frame = self.capture.read()
+        # convert it to texture
+        buf1 = cv2.flip(frame, 0)
+        buf = buf1.tostring()
+        texture1 = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
+        # if working on RASPBERRY PI, use colorfmt='rgba' here instead, but stick with "bgr" in blit_buffer.
+        texture1.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
+        # display image from the texture
+        self.img1.texture = texture1
 
 if __name__ == '__main__':
     RecognitionApp().run()
+    cv2.destroyAllWindows()
+
+'''
+import cv2
+
+cv2.namedWindow("preview")
+vc = cv2.VideoCapture(0)
+
+if vc.isOpened(): # try to get the first frame
+    rval, frame = vc.read()
+else:
+    rval = False
+
+while rval:
+    cv2.imshow("preview", frame)
+    rval, frame = vc.read()
+    key = cv2.waitKey(20)
+    if key == 27: # exit on ESC
+        break
+
+vc.release()
+cv2.destroyWindow("preview")
+'''
