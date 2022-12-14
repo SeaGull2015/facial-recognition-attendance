7cf8aba945045276b742d4112af26d34f50f3e70
SeaGull2104 <wk.zaytsev.dv@gmail.com>
2022-12-14 21:53:13 +0300

    Added personal recognition

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-import cv2 # opencv-python - requires numpy
+import cv2  # opencv-python - requires numpy
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import Image
 from kivy.uix.button import Button
 from kivy.clock import Clock
 from kivy.graphics.texture import Texture
+import numpy
 import face_recognition
+import os
+from datetime import datetime
+
 
 class RecognitionWidget(BoxLayout):
     pass
-    
+
 
 class RecognitionApp(App):
     showCam = False
+    path = 'KnownFaces'
+    images = []
+    classNames = []
+    myList = os.listdir(path)
+    encodeListKnown = []
 
-    def savefile(instance):
-        pass
-
-    def toggleCam(instance, value):
-        instance.showCam = not instance.showCam
-        instance.img1.texture = None
-        pass
     def build(self):
         self.img1 = Image()
         btn1 = Button(text='Save to')
@@ -38,22 +40,46 @@ class RecognitionApp(App):
         layout.add_widget(btn2)
         layout.add_widget(self.img1)
 
+        # get faces to read
+        for cls in self.myList:
+            curImg = cv2.imread(f'{self.path}/{cls}')
+            self.images.append(curImg)
+            self.classNames.append(os.path.splitext(cls)[0])
+        self.encodeListKnown = self.findEncodings(self.images)
         # opencv2 stuffs
         self.capture = cv2.VideoCapture(0)
         Clock.schedule_interval(self.update, 1.0 / 33.0)
         return layout
-        #return RecognitionWidget()
+        # return RecognitionWidget()
 
     def update(self, dt):
-        # display image from cam in opencv window
-        ret, frame = self.capture.read()
-        rgb_frame = frame[:, :, ::-1]
-        face_locations = face_recognition.face_locations(rgb_frame)
-        if (self.showCam):
-            for top, right, bottom, left in face_locations:
-                cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+        success, frame = self.capture.read()  # get the frame
+        rgb_frame = frame[:, :, ::-1]  # BGR2RGB
+        #rgb_frame = cv2.resize(frame, (0, 0), None, 0.25, 0.25) # could do that to make it faster
+        face_locations = face_recognition.face_locations(rgb_frame)  # find faces
+        encoded_faces = face_recognition.face_encodings(rgb_frame, face_locations)
+
+        for encodeFace, faceLoc in zip(encoded_faces, face_locations): # zip to only go through relevant pairs, not all possible ones
+            matches = face_recognition.compare_faces(self.encodeListKnown, encodeFace)
+            faceDis = face_recognition.face_distance(self.encodeListKnown, encodeFace)
+            matchIndex = numpy.argmin(faceDis)  # faster
+
+            if matches[matchIndex]:
+                name = self.classNames[matchIndex]
+                self.markAttendance(name) # this suxxx performance a lot
+                if self.showCam:
+                    for top, right, bottom, left in face_locations:
+                        cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
+                        cv2.putText(frame, name, (left + 6, bottom - 6),
+                                    cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255),2)
+
+            # if (self. showCam): # draw a rectangle around the face
+            #    cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
+
+        # show the camera footage
+        if self.showCam:
             # convert it to texture
-            buf1 = cv2.flip(frame, 0) # this should have the border around the face, but doesn't, ok
+            buf1 = cv2.flip(frame, 0)  # this should have the border around the face, but doesn't, ok
             buf = buf1.tostring()
             texture1 = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
             # if working on RASPBERRY PI, use colorfmt='rgba' here instead, but stick with "bgr" in blit_buffer.
@@ -61,7 +87,33 @@ class RecognitionApp(App):
             # display image from the texture
             self.img1.texture = texture1
 
+    def savefile(instance):
+        pass
 
+    def toggleCam(instance, value):
+        instance.showCam = not instance.showCam
+        instance.img1.texture = None
+        pass
+
+    def findEncodings(self, images):
+        encodeList = []
+        for img in images:
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            encode = face_recognition.face_encodings(img)[0]
+            encodeList.append(encode)
+        return encodeList
+
+    def markAttendance(self, name):
+        with open("Attendance.csv", "r+") as f:
+            myDataList = f.readlines()
+            nameList = []
+            for line in myDataList:
+                entry = line.split(',')
+                nameList.append(entry[0])
+            if name not in nameList:
+                now = datetime.now()
+                dtString = now.strftime("%H:%M:%S")
+                f.writelines(f'\n{name}, {dtString}')
 
 
 if __name__ == '__main__':
